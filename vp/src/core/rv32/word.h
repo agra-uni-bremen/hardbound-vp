6e42f0cada5cd497f2671f31a49b2c631e82e9e9
Sören Tempel <tempel@uni-bremen.de>
2021-02-08 23:06:57 +0100

    rv32-symex: Properly handle pointer edge cases in sub/add

--- vp/src/core/rv32/word.h
+++ vp/src/core/rv32/word.h
@@ -1,6 +1,7 @@
 #ifndef RISCV_VP_WORD_H
 #define RISCV_VP_WORD_H
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <tuple>
@@ -67,6 +68,15 @@ public:
 	}
 
 	Word add(const Word other) const {
+		/* From 6.5.6 (2) in ISO/IEC 9899 (C99):
+		 *
+		 *   For addition, either both operands shall have arithmetic type, or one operand
+		 *   shall be pointer to an object type and the other shall have integer type.
+		 *
+		 * As such, we cannot perform addition on two pointers.
+		 */
+		assert(!(this->is_pointer() && other.is_pointer()));
+
 		auto base = this->base;
 		auto bound = this->bound;
 		if (other.is_pointer())
@@ -84,6 +94,17 @@ public:
 		auto bound = this->bound;
 		if (other.is_pointer())
 			std::tie(base, bound) = other.metadata();
+
+		/* From 6.5.6 (9) in ISO/IEC 9899 (C99):
+		 *
+		 *   When two pointers are subtracted, both shall point to elements of the same
+		 *   array object, or one past the last element of the array object; the result is
+		 *   the difference of the subscripts of the two array elements.
+		 *
+		 * As such, the result must not be a pointer in this case.
+		 */
+		if (this->is_pointer() && other.is_pointer())
+			base = bound = 0;
 
 		return Word(value->sub(other), base, bound);
 	}
